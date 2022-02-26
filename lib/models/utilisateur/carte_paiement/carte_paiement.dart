e5e6daaa5db7e73870588f826aed8cde9742a93c
alpha SOW <alphaseul@yahoo.fr>
2022-02-26 23:57:33 +0100

    card model

--- lib/models/utilisateur/carte_paiement/carte_paiement.dart
+++ lib/models/utilisateur/carte_paiement/carte_paiement.dart
@@ -4,20 +4,22 @@ part 'carte_paiement.g.dart';
 
 @JsonSerializable()
 class CartePaiement {
-  CartePaiement(
+  CartePaiement({
     this.id,
-    this.name,
+    this.type,
+    this.cardType,
     this.cardNumber,
     this.cardExpMonth,
     this.cardExpYear,
     this.cardCvc,
-  );
-  final String id;
-  final String name;
-  final int cardNumber;
-  final int cardExpMonth;
-  final int cardExpYear;
-  final int cardCvc;
+  });
+  final String? id;
+  final String? type;
+  final String? cardType;
+  final int? cardNumber;
+  final int? cardExpMonth;
+  final int? cardExpYear;
+  final int? cardCvc;
 
   factory CartePaiement.fromJson(Map<String, dynamic> json) =>
       _$CartePaiementFromJson(json);
--- lib/models/utilisateur/carte_paiement/carte_paiement.g.dart
+++ lib/models/utilisateur/carte_paiement/carte_paiement.g.dart
@@ -7,18 +7,20 @@ part of 'carte_paiement.dart';
 // **************************************************************************
 
 CartePaiement _$CartePaiementFromJson(Map json) => CartePaiement(
-      json['id'] as String,
-      json['name'] as String,
-      json['cardNumber'] as int,
-      json['cardExpMonth'] as int,
-      json['cardExpYear'] as int,
-      json['cardCvc'] as int,
+      id: json['id'] as String?,
+      type: json['type'] as String?,
+      cardType: json['cardType'] as String?,
+      cardNumber: json['cardNumber'] as int?,
+      cardExpMonth: json['cardExpMonth'] as int?,
+      cardExpYear: json['cardExpYear'] as int?,
+      cardCvc: json['cardCvc'] as int?,
     );
 
 Map<String, dynamic> _$CartePaiementToJson(CartePaiement instance) =>
     <String, dynamic>{
       'id': instance.id,
-      'name': instance.name,
+      'type': instance.type,
+      'cardType': instance.cardType,
       'cardNumber': instance.cardNumber,
       'cardExpMonth': instance.cardExpMonth,
       'cardExpYear': instance.cardExpYear,
