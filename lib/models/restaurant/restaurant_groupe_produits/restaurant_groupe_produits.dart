24d44c7d39278ff7648be8a1ade5cdbb9931af06
alpha SOW <alphaseul@yahoo.fr>
2022-03-17 15:24:32 +0100

    RestaurantGroupeProduits add choix

--- lib/models/restaurant/restaurant_groupe_produits/restaurant_groupe_produits.dart
+++ lib/models/restaurant/restaurant_groupe_produits/restaurant_groupe_produits.dart
@@ -4,20 +4,23 @@ part 'restaurant_groupe_produits.g.dart';
 
 @JsonSerializable()
 class RestaurantGroupeProduits {
-  RestaurantGroupeProduits({
-    this.rank,
-    this.id,
-    this.nom,
-    this.description,
-    this.requis,
-  });
+  RestaurantGroupeProduits(
+      {this.rank,
+      this.id,
+      this.nom,
+      this.description,
+      this.requis,
+      this.choix});
+
   final int? rank;
   final String? id;
   final String? nom;
   final String? description;
   final bool? requis;
+  final bool? choix;
 
   factory RestaurantGroupeProduits.fromJson(Map<String, dynamic> json) =>
       _$RestaurantGroupeProduitsFromJson(json);
+
   Map<String, dynamic> toJson() => _$RestaurantGroupeProduitsToJson(this);
 }
