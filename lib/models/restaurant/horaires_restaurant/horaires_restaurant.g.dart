090742142843c8235dbb7c2ffa0aee316086dded
alpha SOW <alphaseul@yahoo.fr>
2022-04-06 16:03:24 +0200

    add / restaurant

---
+++ lib/models/restaurant/horaires_restaurant/horaires_restaurant.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'horaires_restaurant.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+HorairesRestaurant _$HorairesRestaurantFromJson(Map json) => HorairesRestaurant(
+      ouverture: json['ouverture'] as String?,
+      fermeture: json['fermeture'] as String?,
+    );
+
+Map<String, dynamic> _$HorairesRestaurantToJson(HorairesRestaurant instance) =>
+    <String, dynamic>{
+      'ouverture': instance.ouverture,
+      'fermeture': instance.fermeture,
+    };
