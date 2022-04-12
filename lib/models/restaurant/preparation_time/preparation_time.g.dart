ec44daad25fe5451511881a9572e45798fe8ee3f
sakbly <sofiane.akbly@qrcode-protect.com>
2022-04-12 09:41:21 +0200

    Updated: Preparation time

---
+++ lib/models/restaurant/preparation_time/preparation_time.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'preparation_time.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PreparationTime _$PreparationTimeFromJson(Map json) => PreparationTime(
+      min: json['min'] as num?,
+      max: json['max'] as num?,
+    );
+
+Map<String, dynamic> _$PreparationTimeToJson(PreparationTime instance) =>
+    <String, dynamic>{
+      'min': instance.min,
+      'max': instance.max,
+    };
