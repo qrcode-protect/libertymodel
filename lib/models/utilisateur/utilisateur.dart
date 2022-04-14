4457c871c0cbd6d474b764ce7b9fb50566a59e18
alpha SOW <alphaseul@yahoo.fr>
2022-04-14 12:34:58 +0200

    expose app info

--- lib/models/utilisateur/utilisateur.dart
+++ lib/models/utilisateur/utilisateur.dart
@@ -2,3 +2,4 @@ export 'adresse/adresse.dart';
 export 'carte_paiement/carte_paiement.dart';
 export 'notification_message/notification_message.dart';
 export 'utilisateur/utilisateur.dart';
+export 'app_info/app_info.dart';
